319fc87d1d50b07ddb228e2289fd6e3906180bfa
T-Iwata0910 <t.iwata0910@gmail.com>
2020-02-12 00:28:52 +0900

    エージェントをパッケージ化

---
+++ rl/rlLQTAgentOptions.m
@@ -0,0 +1,23 @@
+function Options = rlLQTAgentOptions(varagin)
+% rlLQTAgentOptions: Create options for LQT control Agent.
+%
+%   OPT = rlLQTAgentOptions returns the default options for rlLQTAgent. 
+%
+%   OPT = rlLQTAgentOptions('Option1',Value1,'Option2',Value2,...) uses name/value
+%   pairs to override the default values for 'Option1','Option2',...
+%
+%   TODO: サポートしている引数を追加
+%   Supported options are:
+%
+% %   EpsilonGreedyExploration            Parameters for Epsilon Greedy exploration
+% %       Epsilon                         Probability threshold for agent to either randomly
+% %                                       select a valid action or select the action that 
+% %                                       maximizes the state-action value function
+% %       EpsilonMin                      Minimum value of Epsilon
+% %       EpsilonDecay                    Decay rate of Epsilon when Epsilon is updated
+% %   SampleTime                          Sample time of the agent
+% %   DiscountFactor                      Discount factor to apply to future rewards during training
+% %
+% %   See also: rlSARSAAgent, rlDDPGAgentOptions, rlPGAgentOptions, rlACAgentOptions
+
+% Copyright 2020 T.Iwata
